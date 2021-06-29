a26aab51e316a52e2d86b6d4287a18307d4b4655
Amos Wenger <amoswenger@gmail.com>
2021-06-29 19:20:32 +0200

    Cleanups, make GET work

--- src/simples3/s3.rs
+++ src/simples3/s3.rs
@@ -79,80 +79,59 @@ impl Bucket {
         let url = format!("{}{}", self.base_url, key);
         debug!("GET {}", url);
 
-        Box::new(futures::future::err(
-            BadHttpStatusError(http::StatusCode::IM_A_TEAPOT).into(),
-        ))
+        let sr = SignedRequest::new(
+            "GET",
+            "s3",
+            &Region::UsEast2,
+            &format!("/{}/{}", self.name, key),
+        );
 
-        // let url2 = url.clone();
-        // let mut request = Request::new(Method::GET, url.parse().unwrap());
-        // if let Some(creds) = creds {
-        //     let mut canonical_headers = String::new();
+        let request = to_http_request(sr, creds.expect("wanted credentails"));
+        let url = request.url().clone();
+        let url2 = url.clone();
 
-        //     if let Some(token) = creds.token().as_ref().map(|s| s.as_str()) {
-        //         request.headers_mut().insert(
-        //             "x-amz-security-token",
-        //             HeaderValue::from_str(token).expect("Invalid `x-amz-security-token` header"),
-        //         );
-        //         canonical_headers
-        //             .push_str(format!("{}:{}\n", "x-amz-security-token", token).as_ref());
-        //     }
-        //     let date = chrono::offset::Utc::now().to_rfc2822();
-        //     let auth = self.auth("GET", &date, key, "", &canonical_headers, "", creds);
-        //     request.headers_mut().insert(
-        //         "Date",
-        //         HeaderValue::from_str(&date).expect("Invalid date header"),
-        //     );
-        //     request.headers_mut().insert(
-        //         "Authorization",
-        //         HeaderValue::from_str(&auth).expect("Invalid authentication"),
-        //     );
-        // }
-
-        // Box::new(
-        //     self.client
-        //         .execute(request)
-        //         .fwith_context(move || format!("failed GET: {}", url))
-        //         .and_then(|res| {
-        //             if res.status().is_success() {
-        //                 let content_length = res
-        //                     .headers()
-        //                     .get_hyperx::<header::ContentLength>()
-        //                     .map(|header::ContentLength(len)| len);
-        //                 Ok((res.into_body(), content_length))
-        //             } else {
-        //                 Err(BadHttpStatusError(res.status()).into())
-        //             }
-        //         })
-        //         .and_then(|(body, content_length)| {
-        //             body.fold(Vec::new(), |mut body, chunk| {
-        //                 body.extend_from_slice(&chunk);
-        //                 Ok::<_, reqwest::Error>(body)
-        //             })
-        //             .fcontext("failed to read HTTP body")
-        //             .and_then(move |bytes| {
-        //                 if let Some(len) = content_length {
-        //                     if len != bytes.len() as u64 {
-        //                         bail!(format!(
-        //                             "Bad HTTP body size read: {}, expected {}",
-        //                             bytes.len(),
-        //                             len
-        //                         ));
-        //                     } else {
-        //                         info!("Read {} bytes from {}", bytes.len(), url2);
-        //                     }
-        //                 }
-        //                 Ok(bytes)
-        //             })
-        //         }),
-        // )
+        Box::new(
+            self.client
+                .execute(request)
+                .fwith_context(move || format!("failed GET: {}", url))
+                .and_then(|res| {
+                    if res.status().is_success() {
+                        let content_length = res
+                            .headers()
+                            .get_hyperx::<header::ContentLength>()
+                            .map(|header::ContentLength(len)| len);
+                        Ok((res.into_body(), content_length))
+                    } else {
+                        Err(BadHttpStatusError(res.status()).into())
+                    }
+                })
+                .and_then(|(body, content_length)| {
+                    body.fold(Vec::new(), |mut body, chunk| {
+                        body.extend_from_slice(&chunk);
+                        Ok::<_, reqwest::Error>(body)
+                    })
+                    .fcontext("failed to read HTTP body")
+                    .and_then(move |bytes| {
+                        if let Some(len) = content_length {
+                            if len != bytes.len() as u64 {
+                                bail!(format!(
+                                    "Bad HTTP body size read: {}, expected {}",
+                                    bytes.len(),
+                                    len
+                                ));
+                            } else {
+                                info!("Read {} bytes from {}", bytes.len(), url2);
+                            }
+                        }
+                        Ok(bytes)
+                    })
+                }),
+        )
     }
 
     pub fn put(&self, key: &str, content: Vec<u8>, creds: &AwsCredentials) -> SFuture<()> {
         let url = format!("{}{}", self.base_url, key);
         debug!("PUT {}", url);
-
-        let now = chrono::offset::Utc::now();
-        let date = now.to_rfc2822();
 
         let mut sr = SignedRequest::new(
             "PUT",
@@ -161,102 +140,10 @@ impl Bucket {
             &format!("/{}/{}", self.name, key),
         );
 
-        sr.add_optional_header("x-amz-security-token", creds.token().as_deref());
         sr.add_header("x-amz-server-side-encryption", "AES256");
-        sr.add_header("date", &date);
-        sr.add_header("content-type", "application/octet-stream");
-        sr.add_header("content-length", &content.len().to_string());
         sr.set_payload(Some(content));
-        sr.sign(&rusoto_signature::credential::AwsCredentials::new(
-            creds.aws_access_key_id(),
-            creds.aws_secret_access_key(),
-            creds.token().clone(),
-            Some(*creds.expires_at()),
-        ));
 
-        for (k, vv) in sr.headers() {
-            for v in vv {
-                trace!("header: {} = {:?}", k, std::str::from_utf8(&v[..]));
-            }
-        }
-
-        let uri = format!(
-            "{}://{}{}?{}",
-            sr.scheme(),
-            sr.hostname(),
-            sr.canonical_uri,
-            sr.canonical_query_string()
-        );
-        debug!("uri = {}", uri);
-
-        let mut request = Request::new(Method::PUT, uri.parse().unwrap());
-        for (k, vv) in sr.headers() {
-            for v in vv {
-                request.headers_mut().insert(
-                    HeaderName::from_bytes(k.as_bytes()).unwrap(),
-                    HeaderValue::from_bytes(v).unwrap(),
-                );
-            }
-        }
-        debug!("request = {:#?}", request);
-
-        let payload = match sr.payload.unwrap() {
-            rusoto_signature::SignedRequestPayload::Buffer(body) => body,
-            rusoto_signature::SignedRequestPayload::Stream(_) => panic!(),
-        };
-        *request.body_mut() = Some(payload.to_vec().into());
-
-        // let mut request = Request::new(Method::PUT, url.parse().unwrap());
-
-        // let content_type = "application/octet-stream";
-        // let now = chrono::offset::Utc::now();
-        // let date = now.to_rfc2822();
-        // let mut canonical_headers = String::new();
-        // let token = creds.token().as_ref().map(|s| s.as_str());
-        // // Keep the list of header values sorted!
-        // for (header, maybe_value) in &[
-        //     ("x-amz-security-token", token),
-        //     ("x-amz-server-side-encryption", Some("AES256")),
-        // ] {
-        //     if let Some(ref value) = maybe_value {
-        //         request.headers_mut().insert(
-        //             *header,
-        //             HeaderValue::from_str(value)
-        //                 .unwrap_or_else(|_| panic!("Invalid `{}` header", header)),
-        //         );
-        //         canonical_headers
-        //             .push_str(format!("{}:{}\n", header.to_ascii_lowercase(), value).as_ref());
-        //     }
-        // }
-
-        // let auth = self.auth(
-        //     "PUT",
-        //     &date,
-        //     key,
-        //     "",
-        //     &canonical_headers,
-        //     content_type,
-        //     creds,
-        // );
-        // request.headers_mut().insert(
-        //     "Date",
-        //     HeaderValue::from_str(&date).expect("Invalid date header"),
-        // );
-        // request
-        //     .headers_mut()
-        //     .set(header::ContentType(content_type.parse().unwrap()));
-        // request
-        //     .headers_mut()
-        //     .set(header::ContentLength(content.len() as u64));
-        // request.headers_mut().set(header::CacheControl(vec![
-        //     // Two weeks
-        //     header::CacheDirective::MaxAge(1_296_000),
-        // ]));
-        // request.headers_mut().insert(
-        //     "Authorization",
-        //     HeaderValue::from_str(&auth).expect("Invalid authentication"),
-        // );
-        // *request.body_mut() = Some(content.into());
+        let request = to_http_request(sr, creds);
 
         Box::new(self.client.execute(request).then(|result| match result {
             Ok(res) => {
@@ -267,10 +154,8 @@ impl Bucket {
                     trace!("PUT failed with HTTP status: {}", res.status());
                     let err = BadHttpStatusError(res.status()).into();
 
-                    trace!("blocking on future...");
                     tokio_compat::runtime::current_thread::TaskExecutor::current()
                         .spawn_local({
-                            trace!("Hello from beyond futures 0.1");
                             res.into_body()
                                 .fold(Vec::new(), |mut body, chunk| {
                                     body.extend_from_slice(&chunk);
@@ -280,12 +165,14 @@ impl Bucket {
                                     panic!("Could not read body: {}", e);
                                 })
                                 .and_then(|bytes: Vec<u8>| {
-                                    trace!("body = {}", std::str::from_utf8(&bytes[..]).unwrap());
+                                    trace!(
+                                        "PUT S3 error: {}",
+                                        std::str::from_utf8(&bytes[..]).unwrap()
+                                    );
                                     Ok(())
                                 })
                         })
                         .unwrap();
-                    trace!("blocking on future... done!");
                     Err(err)
                 }
             }
@@ -295,6 +182,55 @@ impl Bucket {
             }
         }))
     }
+}
+
+fn to_http_request(mut sr: SignedRequest, creds: &AwsCredentials) -> Request {
+    sr.complement();
+    sr.sign(&rusoto_signature::credential::AwsCredentials::new(
+        creds.aws_access_key_id(),
+        creds.aws_secret_access_key(),
+        creds.token().clone(),
+        Some(*creds.expires_at()),
+    ));
+
+    let uri = format!(
+        "{}://{}{}?{}",
+        sr.scheme(),
+        sr.hostname(),
+        sr.canonical_uri,
+        sr.canonical_query_string()
+    );
+    debug!("uri = {}", uri);
+
+    let method = match sr.method() {
+        "GET" => Method::GET,
+        "POST" => Method::POST,
+        "PUT" => Method::PUT,
+        "DELETE" => Method::DELETE,
+        m => panic!("Unsupported method: {}", m),
+    };
+
+    let mut request = Request::new(method, uri.parse().unwrap());
+    for (k, vv) in sr.headers() {
+        for v in vv {
+            request.headers_mut().insert(
+                HeaderName::from_bytes(k.as_bytes()).unwrap(),
+                HeaderValue::from_bytes(v).unwrap(),
+            );
+        }
+    }
+    debug!("request = {:#?}", request);
+
+    if let Some(body) = sr.payload {
+        match body {
+            rusoto_signature::SignedRequestPayload::Buffer(body) => {
+                *request.body_mut() = Some(body.to_vec().into())
+            }
+            rusoto_signature::SignedRequestPayload::Stream(_) => panic!(),
+        }
+    };
+
+    request
 }
 
 #[cfg(test)]
